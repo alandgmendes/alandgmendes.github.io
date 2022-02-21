37cf5bc5c20b5a7572e897fc7748b7cd333baf23
adgmendes@hotmail.com <adgmendes@hotmail.com>
2022-02-21 10:46:40 -0300

    editadp app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editado and furioso.
+          Editado and furioso and moving.
         </p>
         <a
           className="App-link"
