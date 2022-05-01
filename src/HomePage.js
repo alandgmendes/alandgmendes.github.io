22f926f8df7f8ec3e80198aa2acdbe13e2d84024
alandgmendes <alandgmendes@gmail.com>
2022-05-01 15:49:02 -0300

    inserted profile picture

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import profilePicture from './images/profile.jpeg';
 import './HomePage.css';
 
 function HomePage() {
@@ -11,15 +12,11 @@ function HomePage() {
         <div className='main-div'>
           
         <div className='col-card-perfil'>
-        <p>at is Lorem Ipsum?
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-          has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum.         
-          </p>
+        <img className="profile-picture" src={profilePicture} alt="Alandouglas"/>
+        <p className="itens-profile-card">Alandouglas Godinho Mendes</p>
+        <p className="itens-profile-card">Desenvolvedor de software</p>
+        <p className="itens-profile-card">Estudante de física</p>
+        <p className="itens-profile-card">Apaixonado por análise/ciência de dados</p>
         </div>
         <div className='col-center-perfil'>
           <p>at is Lorem Ipsum?
