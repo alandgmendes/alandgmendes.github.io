54466a017491e4a0de9a77ad81687b7f376789fe
alandgmendes <alandgmendes@gmail.com>
2022-05-17 11:43:27 -0300

    added chatbot

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import ChatBot from 'react-simple-chatbot';
 import profilePicture from './images/profile.jpeg'
 import './HomePage.css'
 
@@ -11,28 +12,36 @@ function HomePage() {
         <div className='main-div'>
           
         <div className='col-card-perfil'>
-        <img className="profile-picture" src={profilePicture} alt="Alandouglas"/>
-        <p className="itens-profile-card">Alandouglas Godinho Mendes</p>
-        <p className="itens-profile-card">Desenvolvedor de software</p>
-        <p className="itens-profile-card">Estudante de física</p>
-        <p className="itens-profile-card">Apaixonado por análise/ciência de dados!</p>
-        <a className="icons-profile-card" href="https://www.linkedin.com/in/alandgmendes/" target="_blank" rel="noreferrer"><span><FontAwesomeIcon className="icons-profile-card" icon={faLinkedin} /></span></a>
+          <img className="profile-picture" src={profilePicture} alt="Alandouglas"/>
+          <p className="itens-profile-card">Alandouglas Godinho Mendes</p>
+          <p className="itens-profile-card">Desenvolvedor de software</p>
+          <p className="itens-profile-card">Estudante de física</p>
+          <p className="itens-profile-card">Apaixonado por análise/ciência de dados!</p>
+          <a className="icons-profile-card" href="https://www.linkedin.com/in/alandgmendes/" target="_blank" rel="noreferrer"><span><FontAwesomeIcon className="icons-profile-card" icon={faLinkedin} /></span></a>
+          <ChatBot
+            steps={[
+              {
+                id: '1',
+                message: 'Insira seu CEP:',
+                trigger: '2',
+              },
+              {
+                id: '2',
+                user: true,
+                validator: (value) => {
+                  if (value != 38750000) {
+                    return 'Fora de Presidente Olegário!';
+                  }
+                  return true;
+                },
+                trigger: '1',
+              },
+            ]}
+          />
         </div>
         <div className='col-center-perfil'>
           <p>at is Lorem Ipsum?
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-          has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-          has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
           has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
           took a galley of type and scrambled it to make a type specimen book. It has survived not only 
           five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
@@ -55,26 +64,7 @@ function HomePage() {
           five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
           It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
           passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum. 
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-          has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-          has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-          has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum.         
+          of Lorem Ipsum.        
           </p>
         </div>
         </div>                
