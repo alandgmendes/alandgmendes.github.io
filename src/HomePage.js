fee92dec6e49c0bc2270e47c9c53876cef465234
alandgmendes <alandgmendes@gmail.com>
2022-06-21 14:45:31 -0300

    -seleto api v1

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,16 +2,70 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import ChatBot from 'react-simple-chatbot';
 import profilePicture from './images/profile.jpeg'
+import React, { useState } from 'react';
 import './HomePage.css'
 
 function HomePage() {
+  const [urlRequest, setUrlRequest] = useState("-");
+  const [urlResult, setUrlResult] = useState("");
+  const [clientId, setClientId] = useState("");
+  var myHeaders = new Headers();
+    myHeaders.append("ClientID", clientId);
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+    
+  };
+  const inputUrlChange = (e) =>{
+    setUrlRequest(e.target.value);
+  }
+  const inputClientIdChange = (e) =>{
+    setClientId(e.target.value);
+  }
+  const infoUrl = "https://api-v2.seletobank.com.br/contadigital/info";
+  const extratoUrl = "https://api-v2.seletobank.com.br/contadigital/recebimentos/transacoes/extrato";
+  const runRequest = () =>{
+    fetch(urlRequest, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        const obj = JSON.parse(result);
+        const builder = Object.entries(obj);
+        const structure = builder.map(item =><p key={item[0]+item[1]}><span>{item[0] + ": => "+ item[1]}</span></p>);
+        setUrlResult(structure);
+      })
+    .catch(error => console.log('error', error));
+
+  }
   
   return (
     <div className="App" >
       <body> 
-        <div className='main-div'>
+        <div >
+          <div>
+            <p>info url: </p>
+            <p>{infoUrl}</p>
+          </div>
+          <br />
+          <div>
+            <p>extrato url: </p>
+            <p>{extratoUrl}</p>
+          </div>
+          <div>
+            <p>Digite clientId: </p>
+            <input value={clientId} id="urlInput" onChange={e => inputClientIdChange(e)}/>        
+          </div>
+          <div>
+            <p>Digite url da request:</p>
+            <input value={urlRequest} id="urlInput" onChange={e => inputUrlChange(e)}/>
+          </div>
+        
+        
+        <button onClick={runRequest}>Executar</button>
+        <div>{urlResult}</div>
           
-          <div className='col-card-perfil'>
+          {/*<div className='col-card-perfil'>
             <img className="profile-picture" src={profilePicture} alt="Alandouglas"/>
             <p className="itens-profile-card">Alandouglas Godinho Mendes</p>
             <p className="itens-profile-card">Desenvolvedor de software</p>
@@ -29,7 +83,7 @@ function HomePage() {
                   id: '2',
                   user: true,
                   validator: (value) => {
-                    if (value != 38750000) {
+                    if (value !== 38750000) {
                       return 'Fora de Presidente Olegário!';
                     }
                     return true;
@@ -38,7 +92,7 @@ function HomePage() {
                 },
               ]}
             />
-          </div>
+            </div>*/}
           {/*<div>
           <div className='col-center-perfil'>
             <p>at is Lorem Ipsum?
